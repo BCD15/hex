7a8457ac255a341dd9580f6855a24b968dcb7360
BCD15 <bruno69correadias@gmail.com>
2023-11-24 10:57:55 -0300

    corsheader

--- hex/models.py
+++ hex/models.py
@@ -22,7 +22,7 @@ class Avaliador(models.Model):
     
 class Empresa(models.Model):
     empresa = models.CharField(max_length=99)
-    cnpj = models.DecimalField(max_digits=14, decimal_places=  0)
+    cnpj = models.DecimalField(max_digits=14, decimal_places= 0)
     email = models.EmailField(max_length=254)
     
     def __str__(self):
@@ -32,7 +32,7 @@ class Empresa(models.Model):
 class Equipe(models.Model):
     equipe = models.CharField(max_length=99)
     integrantes = models.CharField(max_length=100)
-    qtdi = models.DecimalField(max_digits=14, decimal_places=  0)
+    qtdi = models.DecimalField(max_digits=14, decimal_places= 0)
     
     def __str__(self):
         return f"{self.equipe} ({self.integrantes}) {self.qtdi}"
