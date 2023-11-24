ebfe1081452aa47c4a35d77e5c3dd8c1d406f07f
BCD15 <bruno69correadias@gmail.com>
2023-11-24 09:54:11 -0300

    create model people

---
+++ hex/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class People(models.Model):
+    nome = models.CharField(max_length=100)
+    idade = models.DecimalField(max_digits=3, decimal_places=0)
+    email = models.EmailField(max_length=254)
+    
+        # def __str__(self):
+        # return f"{self.nome} ({self.idade}) {self.email}"
